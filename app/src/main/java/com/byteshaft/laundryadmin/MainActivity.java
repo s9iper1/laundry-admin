4400d14328f5627d4f7bd7533159033c3230c277
s9iper1 <s9iper1@gmail.com>
2017-02-21 16:35:59 +0500

    remove fav icon

--- app/src/main/java/com/byteshaft/laundryadmin/MainActivity.java
+++ app/src/main/java/com/byteshaft/laundryadmin/MainActivity.java
@@ -37,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
-
-
     }
 
 
